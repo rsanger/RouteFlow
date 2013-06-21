fc5a4f3d429d04ba40f9aaa1b91effc9d0835e60
KitL <clorier@gmail.com>
2013-06-21 21:10:24 +1200

    RFRoutes: removes lock and stores routes based on dp
      Changes to store the route by dp_port rather than vm, to access
      all routes, use multiple accesses

--- rfserver/rfroutes.py
+++ rfserver/rfroutes.py
@@ -1,4 +1,3 @@
-import threading
 from rflib.ipc.RFProtocol import *
 from rflib.types.Match import *
 from rflib.defs import *
@@ -9,31 +8,35 @@ class RFRoutes:
         # the dicts contain routemods keyed by RFMT_IPV4 values
         # TODO: IPV6
         self.routes = {}
-        self.routeslock = threading.Lock()
 
-    def new_route(self, rm):
-        vm_id = rm.get_id()
+    def new_route(self, rm, ct_id, dp_id, dp_port):
         # TODO: this needs to be more resilient
         match = Match.from_dict(rm.get_matches()[0])
+        key = (ct_id, dp_id, dp_port)
         if match._type != RFMT_IPV4:
             # some kind of error here?
             return
-        with self.routeslock:
-            if not vm_id in self.routes:
-                self.routes[vm_id] = {}
+            if not key in self.routes:
+                self.routes[(ct_id, dp_id, dp_port)] = {}
             if rm.get_mod() is RMT_DELETE:
-                if match.get_value() in self.routes[vm_id]:
-                    del self.routes[vm_id][match.get_value()]
+                if match.get_value() in self.routes[key]:
+                    del self.routes[key][match.get_value()]
             else:
-                self.routes[vm_id][match.get_value()] = rm
+                self.routes[key][match.get_value()] = rm
 
-    def delete_route(self, vm_id, match):
-        with self.routeslock:
-            if match._type == RFMT_IPV4:
-                if match.get_value() in self.routes[vm_id]:
-                    del self.routes[vm_id][match.get_value()]
+    def delete_route(self, ct_id, dp_id, dp_port, match):
+        if match._type == RFMT_IPV4:
+            if match.get_value() in self.routes[(ct_id, dp_id, dp_port)]:
+                del self.routes[(ct_id, dp_id, dp_port)][match.get_value()]
 
-    def get_routes(self, vm_id):
-        with self.routeslock:
-            result = self.routes[vm_id].values()
+    def get_routes(self, ct_id, dp_id, dp_port):
+        result = self.routes[(ct_id, dp_id, dp_port)].values()
+        return result
+
+    # TODO: make this work properly
+    def get_routes_to(self, ct_id, dp_id):
+        result = []
+        for ct, dp, pt in self.routes:
+            if ct == ct_id and dp == dp_id:
+                result.append(self.routes[ct, dp, pt])
         return result
