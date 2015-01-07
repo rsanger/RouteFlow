c441d83c7b8eac873830dcef3364373a508e6133
Christopher Lorier <chris.lorier@reannz.co.nz>
2015-01-07 15:27:57 +1300

    Add additional logging

--- rfclient/FlowTable.cc
+++ rfclient/FlowTable.cc
@@ -433,6 +433,7 @@ void FlowTable::updateRouteTable(  struct rtnl_route *route,
     memset(intf, 0, IF_NAMESIZE + 1);
 
     if (rtnl_route_get_table(route) != RT_TABLE_MAIN) {
+        syslog(LOG_DEBUG, "received route with invalid table, ignoring");
         return;
     }
 
@@ -444,6 +445,7 @@ void FlowTable::updateRouteTable(  struct rtnl_route *route,
         family = IPV6;
         break;
     default:
+        syslog(LOG_DEBUG, "received route with invalid family, ignoring");
         return;
     }
 
@@ -469,10 +471,12 @@ void FlowTable::updateRouteTable(  struct rtnl_route *route,
     rtnl_link_i2name(link_cache, ifindex, intf, sizeof(intf));
 
     if (strcmp(intf, DEFAULT_RFCLIENT_INTERFACE) == 0) {
+        syslog(LOG_DEBUG, "received route for rfclient interface, ignoring");
         return;
     }
 
     if (getInterface(intf, "route", &rentry->interface) != 0) {
+        syslog(LOG_DEBUG, "unable to retrieve interface for route, ignoring");
         return;
     }
 
@@ -501,6 +505,8 @@ void FlowTable::updateRouteTable(  struct rtnl_route *route,
                    net.c_str(), mask.c_str(), gw.c_str());
             this->pendingRoutes.push(PendingRoute(RMT_DELETE, *rentry));
             break;
+        default:
+            syslog(LOG_DEBUG, "route with invalid action, ignoring");
     }
 
     return;
